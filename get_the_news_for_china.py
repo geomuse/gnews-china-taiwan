369ddb6e9553e5af181e63d0cb5fd8c31d91d3a2
geo <34396454+geomuse@users.noreply.github.com>
2024-01-04 15:48:52 +0800

    Add files via upload

---
+++ get_the_news_for_china.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+import requests
+
+from bs4 import BeautifulSoup
+import time , re 
+import os , sys
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(current_dir, "..\..")
+path = r'c:\Users\boonh\Downloads\genv'
+sys.path.append(path)
+
+from notification_bot.telegram_chat import telegram_send
+from notification_bot.loguru_notification import loguru_notf
+logger = loguru_notf(current_dir)
+logger.add('history')
+
+from data_collection_bot.http_status import check_status
+
+# 设置Chrome浏览器驱动路径
+driver_path = 'C:/Users/boonh/Downloads/genv/data_collection_bot/geckodriver-v0.33.0-linux64'
+
+@dataclass
+class news_download:
+
+    def init_browser(self):
+        service = Service(executable_path=driver_path)
+        options = webdriver.FirefoxOptions()
+        return service , options
+
+    def run(self,url):
+        # service , options = self.init_browser()
+        browser = webdriver.Firefox()
+        browser.get(url)
+
+    def get(self,url):
+        response = requests.get(url)
+        if check_status(response.status_code) in [200]:
+            soup = BeautifulSoup(response.text,'html.parser')
+            title = soup.find_all('div',{'class':'part_pictxt_3 lazyload'})
+            # a_href = soup.find_previous(attrs=)
+            text = f"{title[0].text}"
+            text = text.split()
+
+        for _ , __ in enumerate(text):
+            print(_ , __)    
+
+if __name__ == '__main__':
+
+    url = 'https://www.ettoday.net/news/focus/%E5%9C%8B%E9%9A%9B/'
+    nd = news_download()
+    nd.get(url)
+
+    # ts = telegram_send()
+    # ts.send_message('200.')
+    """
+    - 欠a link 切入内文 > 也可以放置的app上.
+    - 做个微小型的文字 mining.
+    """
